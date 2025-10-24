b14ce1c77f1cec244116ef6cae81393eab5a7331
Dmitry Volodin <dv@nocproject.org>
2025-10-24 10:11:43 +0200

    Use NOC images from ghcr.io/gufolabs/noc

--- src/gufo/thor/services/noc.py
+++ src/gufo/thor/services/noc.py
@@ -46,7 +46,7 @@ class NocService(BaseService):
         tag = config.noc.tag
         if svc and svc.tag:
             tag = svc.tag
-        return f"gufolabs/noc:{tag}"
+        return f"ghcr.io/gufolabs/noc:{tag}"
 
     def get_compose_command(
         self: "NocService", config: Config, svc: Optional[ServiceConfig]
