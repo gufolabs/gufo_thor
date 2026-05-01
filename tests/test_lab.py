8d57557a3f530af9f37e3db1e731a50dc40c36b4
Dmitry Volodin <dv@nocproject.org>
2026-05-01 08:04:22 +0200

    Update test_lab.py
    Fix

--- tests/test_lab.py
+++ tests/test_lab.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------
 # Gufo Thor: Lab tests
 # ---------------------------------------------------------------------
-# Copyright (C) 2023-25, Gufo Labs
+# Copyright (C) 2023-26, Gufo Labs
 # ---------------------------------------------------------------------
 
 # Gufo Thor modules
@@ -9,7 +9,7 @@ from gufo.thor.labs.base import BaseLab
 
 
 def test_vyos_docker_console_args() -> None:
-    lab = BaseLab.get("vyos")
+    lab = BaseLab.get("vyos15")
     cargs = lab.get_docker_console_args()
     assert cargs
     assert cargs.args == ["-u", "vyos"]
